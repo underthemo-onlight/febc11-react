938b6cf5606ce836c1287aaf7197360bb008bf57
Kilyong Jeong <uzoolove@gmail.com>
2024-11-13 14:50:50 +0900

    🚧

--- workspace-ins/ch01-buildup/js-review/06-01.js
+++ workspace-ins/ch01-buildup/js-review/06-01.js
@@ -17,7 +17,11 @@ function f2(f1Result){
 function test(){
   const f1Result = f1();
   const result = f2(f1Result);
+<<<<<<< HEAD
   console.log('8.', result);
+=======
+  console.log(result);
+>>>>>>> 7aac1ee (🚧)
 }
 
 console.log('1. 테스트 시작.');
