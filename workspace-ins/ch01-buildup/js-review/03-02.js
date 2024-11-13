f3905b3b4ac17b4017428603ce9acc2b880d7fe7
Kilyong Jeong <uzoolove@gmail.com>
2024-11-13 11:51:02 +0900

    🚧

--- workspace-ins/ch01-buildup/js-review/03-02.js
+++ workspace-ins/ch01-buildup/js-review/03-02.js
@@ -3,6 +3,7 @@ var item = { no: 1, todo: '두부', done: true };
 // 1. 대입 연산자로 newItem 생성
 var newItem = item;
 
+<<<<<<< HEAD
 // 2. Object.assign() 사용해서 속성 추가
 // Object.assign(target, ...source): target 객체에 source 객체들의 속성을 추가해서 target을 반환
 var newItem = Object.assign(item, { delete: true });
@@ -16,5 +17,13 @@ var newItem = { ...item, done: false };
 
 // item, newItem 비교
 // newItem.done = false;
+=======
+// 2. Object.assing() 사용해서 속성 추가
+// Object.assign(target, ...source)
+var newItem = Object.assign();
+
+// item, newItem 비교
+newItem.done = false;
+>>>>>>> cb046ea (🚧)
 console.log(item, newItem);
 console.log('같은 객체인가?', item === newItem);
