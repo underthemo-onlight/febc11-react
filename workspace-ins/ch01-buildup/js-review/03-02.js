01d793669d969a5545445a15ad5b2a9230584e50
Kilyong Jeong <uzoolove@gmail.com>
2024-11-13 13:14:58 +0900

    ✨

--- workspace-ins/ch01-buildup/js-review/03-02.js
+++ workspace-ins/ch01-buildup/js-review/03-02.js
@@ -4,6 +4,9 @@ var item = { no: 1, todo: '두부', done: true };
 var newItem = item;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> dec4c4b (✨)
 // 2. Object.assign() 사용해서 속성 추가
 // Object.assign(target, ...source): target 객체에 source 객체들의 속성을 추가해서 target을 반환
 var newItem = Object.assign(item, { delete: true });
@@ -14,6 +17,7 @@ var newItem = { no: item.no, done: item.done, todo: item.todo };
 
 // 4. 전개 연산자를 이용한 복사
 var newItem = { ...item, done: false };
+<<<<<<< HEAD
 
 // item, newItem 비교
 // newItem.done = false;
@@ -25,5 +29,10 @@ var newItem = Object.assign();
 // item, newItem 비교
 newItem.done = false;
 >>>>>>> cb046ea (🚧)
+=======
+
+// item, newItem 비교
+// newItem.done = false;
+>>>>>>> dec4c4b (✨)
 console.log(item, newItem);
 console.log('같은 객체인가?', item === newItem);
