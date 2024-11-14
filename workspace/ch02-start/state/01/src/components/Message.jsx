fe7d88e58a8b54f1862e9eed4a4aaea3610334c3
underthemo-onlight <underthemo_onlight@naver.com>
2024-11-14 16:03:25 +0900

    state 대신 컴포넌트 외부의 변수(모듈 스코프 변수) 사용시 문제점

---
+++ workspace/ch02-start/state/01/src/components/Message.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+// 모듈 스코프 변수 X
+// let count = 0;
+
+export default function Message() {
+  console.log("Message 렌더링");
+
+  //화면이 리랜더링 => 상태로 관리
+  //state 사용
+  const [count, setCount] = useState(0);
+  const [msg, setMsg] = useState("");
+
+  const handleChange = (event) => {
+    const inputMsg = event.target.value;
+    setMsg(inputMsg);
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <br />
+      <span>입력 메세지: {msg}</span>
+      <br />
+      <span>입력 횟수: {count}</span>
+    </div>
+  );
+}
