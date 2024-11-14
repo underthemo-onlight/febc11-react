a1578f4695f8da050e2538c14a8f03dcf9c5326b
Kilyong Jeong <uzoolove@gmail.com>
2024-11-14 14:50:32 +0900

    🚧

--- workspace-ins/ch02-start/state/03/src/App.jsx
+++ workspace-ins/ch02-start/state/03/src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useState } from "react";
 
 function App(){
@@ -9,10 +10,19 @@ function App(){
 function App(){
   
 >>>>>>> 8246e5e (🚧)
+=======
+import { useState } from "react";
+
+function App(){
+
+  const [position, setPosition] = useState({ x: 100, y: 250 });
+
+>>>>>>> fb6cdec (🚧)
   return (
     <>
       <h1>03 상태관리 대상이 객체일 경우 주의 사항</h1>
       <div
+<<<<<<< HEAD
 <<<<<<< HEAD
         onPointerMove={ event => {
           // 속성을 직접 수정하면 리렌더링되지 않음
@@ -27,6 +37,12 @@ function App(){
         } }
 =======
 >>>>>>> 8246e5e (🚧)
+=======
+        onPointerMove={ event => {
+          position.x = event.clientX;
+          position.y = event.clientY;
+        } }
+>>>>>>> fb6cdec (🚧)
         style={{
           position: 'relative',
           width: '100vw',
@@ -37,10 +53,14 @@ function App(){
           backgroundColor: 'red',
           borderRadius: '50%',
 <<<<<<< HEAD
+<<<<<<< HEAD
           transform: `translate(${position.x-10}px, ${position.y-10}px)`,
 =======
           transform: `translate(${10}px, ${10}px)`,
 >>>>>>> 8246e5e (🚧)
+=======
+          transform: `translate(${position.x-10}px, ${position.y-10}px)`,
+>>>>>>> fb6cdec (🚧)
           left: -10,
           top: -10,
           width: 20,
