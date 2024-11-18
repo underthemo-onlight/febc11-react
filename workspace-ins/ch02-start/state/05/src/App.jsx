6ca57d5e730e7358eb2a82f3317b358d48846463
Kilyong Jeong <uzoolove@gmail.com>
2024-11-18 13:51:31 +0900

    🚧

--- workspace-ins/ch02-start/state/05/src/App.jsx
+++ workspace-ins/ch02-start/state/05/src/App.jsx
@@ -148,8 +148,11 @@ function App() {
 import { useState } from "react";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 345a303 (🚧)
 =======
+=======
+>>>>>>> 4366de0 (🚧)
 const errorStyle = {
   fontSize: '12px',
   color: 'red',
@@ -159,6 +162,7 @@ const errorStyle = {
 const emailExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const cellphoneExp = /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/;
 
+<<<<<<< HEAD
 >>>>>>> 4366de0 (🚧)
 function App() {
 
@@ -296,6 +300,8 @@ function App() {
 import { useState } from "react";
 
 >>>>>>> 345a303 (🚧)
+=======
+>>>>>>> 4366de0 (🚧)
 function App() {
 
   // const [name, setName] = useState('');
@@ -307,6 +313,8 @@ function App() {
     email: '',
     cellphone: '010',
   });
+
+  const [errors, setErrors] = useState({});
 
   // const handleNameChange = (event) => {
   //   setName(event.target.value);
@@ -326,6 +334,45 @@ function App() {
     setUser(newUser);
   };
 
+  const onSubmit = (event) => {
+    // 브라우저의 기본 동작 취소(submit 동작 취소)
+    event.preventDefault();
+
+    let newErrors;
+    // 입력 메세지 검증 작업
+    if(user.name.trim() === ''){
+      newErrors = {
+        name: { message: '이름을 입력하세요.' }
+      };
+    }else if(user.name.trim().length < 2){
+      newErrors = {
+        name: { message: '2글자 이상 입력하세요.' }
+      };
+    }else if(user.email.trim() === ''){
+      newErrors = {
+        email: { message: '이메일을 입력하세요.' }
+      };
+    }else if(user.cellphone.trim() === ''){
+      newErrors = {
+        cellphone: { message: '휴대폰 번호를 입력하세요.' }
+      };
+    }else if(emailExp.test(user.email)){
+      newErrors = {
+        cellphone: { message: '이메일 양식에 맞지 않습니다.' }
+      };
+    }else if(cellphoneExp.test(user.email)){
+      newErrors = {
+        cellphone: { message: '휴대폰 형식에 맞지 않습니다.' }
+      };
+    }
+
+    if(newErrors){
+      setErrors(newErrors);
+    }else{
+      setErrors({});
+    }
+  };
+
   return (
     <>
 <<<<<<< HEAD
@@ -334,7 +381,7 @@ function App() {
 =======
       <h1>05 회원가입 입력값 상태 관리</h1>
 
-      <form>
+      <form onSubmit={ onSubmit }>
         <label htmlFor="name">이름</label>
         <input 
           id="name"
@@ -342,7 +389,7 @@ function App() {
           value={ user.name }
           onChange={ handleChange }
         /><br/>
-        <div>검증 실패 메세지</div>
+        <div style={ errorStyle }>{ errors.name?.message }</div>
 
         <label htmlFor="email">이메일</label>
         <input 
@@ -351,7 +398,7 @@ function App() {
           value={ user.email }
           onChange={ handleChange }
         /><br/>
-        <div>검증 실패 메세지</div>
+        <div style={ errorStyle }>{ errors.email?.message }</div>
 
         <label htmlFor="cellphone">휴대폰</label>
         <input 
@@ -360,7 +407,7 @@ function App() {
           value={ user.cellphone }
           onChange={ handleChange }
         /><br/>
-        <div>검증 실패 메세지</div>
+        <div style={ errorStyle }>{ errors.cellphone?.message }</div>
 
         <button type="submit">가입</button>
       </form>
