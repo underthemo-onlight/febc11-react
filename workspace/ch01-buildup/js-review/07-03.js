02fe3d0f6f2b3c5af7efb8d583d48368a047f93d
underthemo-onlight <underthemo_onlight@naver.com>
2024-11-19 11:47:32 +0900

    배열 요소에서 2와 3의 최소 공배수 구하기

--- workspace/ch01-buildup/js-review/07-03.js
+++ workspace/ch01-buildup/js-review/07-03.js
@@ -1,7 +1,10 @@
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var array = [6, 7, 8, 1, 4, 5, 2, 3, 9, 10];
+// -를 반환할 경우 a, b 정렬
+// +를 반환할 경우 b, a 정렬
+// =을 반환할 경우 그대로 
+array.sort((a, b) => a - b);
+console.log(array);
 
-var result = array.find((item) => {
+var result = array.find(num => num % 2 === 0 && num % 3 === 0);
 
-})
-
-console.log(result);
+console.log(result); // 6
