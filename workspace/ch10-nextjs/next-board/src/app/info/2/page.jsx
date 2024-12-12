dd594ff000c18ee11061d5175ab617a34f0f7b53
underthemo-onlight <underthemo_onlight@naver.com>
2024-12-12 16:31:25 +0900

    Next.js

---
+++ workspace/ch10-nextjs/next-board/src/app/info/2/page.jsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex items-center justify-center" style="height: 300px">
+        <div className="text-center">
+          <h3 className="mb-4 text-lg font-semibold">잠시만 기다려주세요.</h3>
+          <span>로딩중...</span>
+        </div>
+      </div>
+    </>
+  );
+}
